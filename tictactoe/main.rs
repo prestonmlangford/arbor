3d5fc374dc404ba7ecdc380c4c5285e91b819fe7
prestonmlangford <prestonmlangford@gmail.com>
2021-01-10 17:33:36 -0600

    more progress on tictactoe

--- tictactoe/main.rs
+++ tictactoe/main.rs
@@ -4,8 +4,8 @@ use std::fmt;
 use mcts::randxorshift::RandXorShift;
 use rand::{Rng,FromEntropy};
 
-#[derive(Copy,Clone,PartialEq)]
-enum Mark {X,O,N}
+#[derive(Copy,Clone,PartialEq,Debug)]
+enum Mark {N,X,O}
 
 impl Display for Mark {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
@@ -17,12 +17,26 @@ impl Display for Mark {
     }
 }
 
-type Move = usize;
+#[derive(Copy,Clone,Debug)]
+enum Move {
+    TL,TM,TR,
+    ML,MM,MR,
+    BL,BM,BR
+}
 
+use Move::*;
+static ALLMOVES: [Move;9] = [
+    TL,TM,TR,
+    ML,MM,MR,
+    BL,BM,BR
+];
+
+#[derive(Copy,Clone,Debug)]
 struct TicTacToe {
     space: [Mark;9],
     turn: usize,
     side: Mark,
+    hash: u64,
 }
 
 
@@ -66,8 +80,6 @@ impl TicTacToe {
         Mark::N
     }
 
-    
-
     fn make(&self, m: Move) -> Option<Self> {
         if self.winner() != Mark::N {
             return None;
@@ -81,34 +93,84 @@ impl TicTacToe {
             space: self.space,
             turn: self.turn + 1,
             side: if self.side == Mark::X {Mark::O} else {Mark::X},
+            hash: self.hash | ((self.side as u64) << 2*(m as u64)),
         };
 
-        next.space[m] = next.side;
+        next.space[m as usize] = next.side;
 
         Some(next)
+    }
+
+    fn legal_moves(&self) -> Vec<Move> {
+        let mut result = Vec::new();
+        for i in 0..9 {
+            if self.space[i] == Mark::N {
+                result.push(ALLMOVES[i])
+            }
+        }
+        result
     }
 }
 
 
 fn rmove(state: &TicTacToe, rand: &mut RandXorShift) -> Option<Move> {
-    let mut m: Vec<Move> = (0..10).collect();
+    let mut moves: Vec<&Move> = ALLMOVES.iter().collect();
 
-    while m.len() > 0 {
-        let r = rand.gen_range(0,m.len());
-        if state.space[r] == Mark::N {
-            return Some(r);
+    while moves.len() > 0 {
+        let r = rand.gen_range(0,moves.len());
+        let m = *moves[r];
+        if state.space[m as usize] == Mark::N {
+            return Some(m);
         } else {
-            m.swap_remove(r);
+            moves.swap_remove(r);
         }
     }
 
     None
 }
 
+#[derive(Debug)]
 struct StateManager {
-
+    stack: Vec<TicTacToe>,
 }
 
+impl StateManager {
+    fn cur(&self) -> &TicTacToe {
+        self.stack.last().unwrap()
+    }
+}
+
+
+impl mcts::Action for Move {}
+
+
+impl mcts::GameState<Move> for StateManager {
+    fn value(&self) -> f32 {
+        0.0 //PMLFIXME do random rollout
+    }
+    
+    fn actions(&self) -> Vec<Move> {
+        self.cur().legal_moves()
+    }
+
+    fn make(&mut self,action: Move) {
+        if let Some(next) = self.cur().make(action) {
+            self.stack.push(next);
+        } else {
+            panic!("Make called with bad move")
+        }
+    }
+
+    fn unmake(&mut self) {
+        if self.stack.len() > 1 {
+            self.stack.pop();
+        }
+    }
+
+    fn hash(&self) -> u64 {
+        self.cur().hash
+    }
+}
 
 fn main(){
 
