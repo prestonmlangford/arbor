bc957b1ae9ed65fc93c8fba6eb9309f3a2336bab
prestonmlangford <prestonmlangford@gmail.com>
2021-12-30 18:38:15 -0600

    seperated game trait from game loop

--- connect4/main.rs
+++ connect4/main.rs
@@ -4,237 +4,13 @@ extern crate arbor;
 extern crate rand;
 extern crate rand_xorshift;
 
+mod connect4;
 
 use std::io;
 use std::io::prelude::*;
-use std::fmt::Display;
-use std::fmt;
 use std::time::Duration;
+use connect4::*;
 use arbor::*;
-use rand_xorshift::XorShiftRng as Rand;
-use rand::{RngCore,SeedableRng};
-
-const W: usize = 7;
-const H: usize = 6;
-
-
-lazy_static!{
-    static ref ZTABLE: [u64;2*W*H] = {
-        let mut table = [0;2*W*H];
-        let seed = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16];
-        let mut rand = Rand::from_seed(seed);
-        for entry in table.iter_mut() {
-            *entry = rand.next_u64();
-        }
-        table
-    };
-}
-const ZTURN: u64 = 0x123456789ABCDEF0;
-
-#[derive(Debug,Copy,Clone)]
-enum Column {C1,C2,C3,C4,C5,C6,C7}
-use Column::*;
-const COL: [Column;7] = [C1,C2,C3,C4,C5,C6,C7];
-
-#[derive(Debug,Copy,Clone,PartialEq)]
-enum Disc {N,R,Y}
-
-impl Display for Disc {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self {
-            Self::R => write!(f,"R"),
-            Self::Y => write!(f,"Y"),
-            Self::N => write!(f,"-"),
-        }
-    }
-}
-
-#[derive(Debug,Copy,Clone)]
-struct Connect4 {
-    space: [Disc; W*H],
-    gameover: bool,
-    side: bool,
-    winner: Disc,
-    hash: u64,
-}
-
-const NEWGAME: Connect4 = 
-    Connect4 {
-        space: [Disc::N;W*H],
-        gameover: false,
-        side: true,
-        winner: Disc::N,
-        hash: 0,
-    };
-
-impl Display for Connect4 {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let mut result = String::new();
-        result.push_str(&format!("{}\n",if self.side {"R"} else {"Y"}));
-        
-        for _ in 0..W {
-            result.push_str("----")
-        }
-        result.push_str("-\n");
-        
-        for h in 0..H {
-            let r = H - 1 - h;
-            for w in 0..W {
-                result.push_str(&format!("| {} ",self.space[w + r*W]))
-            }
-            result.push('|');
-            result.push('\n');
-        }
-        
-        for _ in 0..W {
-            result.push_str("----")
-        }
-        result.push_str("-\n");
-        
-        for c in 0..W {
-            result.push_str(&format!("  {} ",c + 1))
-        }
-        result.push_str(" \n");
-        
-        
-        
-        write!(f,"{}",result)
-    }
-}
-
-impl Connect4 {
-    fn new() -> Self {
-        NEWGAME
-    }
-
-    #[allow(dead_code)]
-    fn load(moves: &[Column]) -> Connect4 {
-        let mut g = Self::new();
-        for m in moves {
-            println!("{}",g);
-            g = g.make(*m);
-        }
-        println!("{}",g);
-        g
-    }
-    
-    fn count(&self,dr: i32, dc: i32, mut r: i32, mut c: i32) -> u32 {
-        let color = if self.side {Disc::R} else {Disc::Y};
-        let mut result = 0;
-        loop {
-            r += dr;
-            c += dc;
-            let bounds = 
-                (r >= H as i32) ||
-                (c >= W as i32) ||
-                (r < 0) ||
-                (c < 0);
-        
-            if bounds {
-                break;
-            }
-            
-            if self.space[(r*(W as i32) + c) as usize] == color {
-                result += 1;
-            } else {
-                break;
-            }
-        }
-        
-        result
-    }
-    
-    fn check_n(&self,r: usize, c: usize) -> bool {
-        let u = self.count( 1, 0, r as i32, c as i32);
-        let d = self.count(-1, 0, r as i32, c as i32);
-        u + d >= 3
-    }
-    
-    fn check_e(&self,r: usize, c: usize) -> bool {
-        let l = self.count(0,-1, r as i32, c as i32);
-        let r = self.count(0, 1, r as i32, c as i32);
-        l + r >= 3
-    }
-    
-    fn check_nw(&self,r: usize, c: usize) -> bool {
-        let ul = self.count( 1,-1, r as i32, c as i32);
-        let dr = self.count(-1, 1, r as i32, c as i32);
-        ul + dr >= 3
-    }
-    
-    fn check_ne(&self,r: usize, c: usize) -> bool {
-        let ur = self.count( 1, 1, r as i32, c as i32);
-        let dl = self.count(-1,-1, r as i32, c as i32);
-        ur + dl >= 3
-    }
-    
-}
-
-impl Player for Disc {}
-impl Action for Column {}
-
-impl GameState<Disc,Column> for Connect4 {
-    
-    fn actions<F>(&self,f: &mut F) where F: FnMut(Column){
-        for c in 0..W {
-            if self.space[(H - 1)*W + c] == Disc::N {
-                f(COL[c]);
-            }
-        }
-    }
-    
-    fn make(&self,c: Column) -> Self {
-        let column = c as usize;
-        debug_assert!(column < W,"make called with invalid column {}", column);
-        let color = if self.side {Disc::R} else {Disc::Y};
-        let mut next = *self;
-        next.side = !self.side;
-        
-        let mut row = 0;
-        while row < H {
-            let i = row*W + column;
-            if next.space[i] == Disc::N {
-                next.space[i] = color;
-                next.hash ^= if next.side {ZTABLE[i]} else {ZTABLE[i + W*H]};
-                next.hash ^= ZTURN;
-                break;
-            }
-            row += 1;
-        }
-        debug_assert!(row < H,"make called on invalid column {}",column);
-        
-        let win = 
-            self.check_n(row, column) ||
-            self.check_e(row, column) ||
-            self.check_nw(row, column) ||
-            self.check_ne(row, column);
-        
-        let mut full = true;
-        next.actions(&mut |_|{full = false;});
-        
-        next.gameover = win || full;
-        next.winner = if win {color} else {Disc::N};
-        
-        next
-    }
-
-    fn gameover(&self) -> Option<GameResult> {
-        if self.gameover {
-            if self.winner == Disc::N {
-                Some(GameResult::Draw)
-            } else {
-                // side to play last always wins
-                Some(GameResult::Lose)
-            }
-        } else {
-            None
-        }
-    }
-
-    fn player(&self) -> Disc {
-        if self.side {Disc::R} else {Disc::Y}
-    }
-}
 
 fn main() {
     println!("Connect 4!");
@@ -244,7 +20,7 @@ fn main() {
     let mut gamestate = Connect4::load(&game);
     
     loop {
-        if !gamestate.side {
+        if gamestate.player() == Disc::Y {
             print!("=> ");
             //flushes standard out so the print statements are actually displayed
             io::stdout().flush().unwrap();
@@ -290,12 +66,12 @@ fn main() {
         println!("{}",gamestate);
         
         
-        if gamestate.gameover {
-            println!("gameover!");
-            let winner = gamestate.winner;
-            match winner {
-                Disc::R => println!("Red wins"),
-                Disc::Y => println!("Yellow wins"),
+        if let Some(result) = gamestate.gameover() {
+            println!("gameover! {:?}",result);
+            
+            match gamestate.player() {
+                Disc::R => println!("Red"),
+                Disc::Y => println!("Yellow"),
                 Disc::N => println!("Stalemate"),
             }
             break;
---
+++ mancala/mancala.rs
@@ -0,0 +1,444 @@
+use std::fmt::Display;
+use std::fmt;
+
+use arbor::*;
+use rand_xorshift::XorShiftRng as Rand;
+use rand::{RngCore,SeedableRng};
+
+#[derive(Copy,Clone,PartialEq,Debug)]
+pub enum Player {L,R}
+
+impl Player {
+    fn other(&self) -> Self {
+        match self {
+            Self::L => Self::R,
+            Self::R => Self::L,
+        }
+    }
+}
+
+#[allow(dead_code)]
+#[derive(Copy,Clone,PartialEq,Debug)]
+pub enum Pit {
+    R1,R2,R3,R4,R5,R6,RBank,
+    L1,L2,L3,L4,L5,L6,LBank,
+}
+use Pit::*;
+
+pub const RB: usize = RBank as usize;
+pub const LB: usize = LBank as usize;
+pub const NP: usize = 2*(LBank as usize - RBank as usize);
+pub const NS: usize = 4*(NP - 2);
+pub const PIT: [Pit; NP] = [
+    R1,R2,R3,R4,R5,R6,RBank,
+    L1,L2,L3,L4,L5,L6,LBank,
+];
+
+lazy_static!{
+    static ref ZTABLE: [u64;NP*NS] = {
+        let mut table = [0;NP*NS];
+        let mut rand = Rand::from_seed([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]);
+        //let mut rand = rand::thread_rng();
+        for entry in table.iter_mut() {
+            *entry = rand.next_u64();
+        }
+        table
+    };
+}
+const ZTURN: u64 = 0x123456789ABCDEF0;
+
+#[derive(Copy,Clone,Debug)]
+pub struct Mancala {
+    pit: [u8; NP],
+    side: Player,
+}
+lazy_static!{
+    static ref NEWGAME: Mancala = Mancala::new();
+}
+
+impl Display for Mancala {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f,
+"
+{}
+-------------------------
+|  |{:2}|{:2}|{:2}|{:2}|{:2}|{:2}|  |
+|{:2}|--|--|--|--|--|--|{:2}|
+|  |{:2}|{:2}|{:2}|{:2}|{:2}|{:2}|  |
+-------------------------
+     1  2  3  4  5  6
+",
+            if self.side == Player::L {"Left Player"} else {"Right Player"},
+            self.pit[L6 as usize],self.pit[L5 as usize],self.pit[L4 as usize],self.pit[L3 as usize],self.pit[L2 as usize],self.pit[L1 as usize],
+            self.pit[LB],self.pit[RB],
+            self.pit[R1 as usize],self.pit[R2 as usize],self.pit[R3 as usize],self.pit[R4 as usize],self.pit[R5 as usize],self.pit[R6 as usize],
+        )
+    }
+}
+
+#[inline]
+fn add(a: usize, b: usize) -> usize {
+    debug_assert!(a < NP,"add(a,b) arg a = {} >= NP = {}",a,NP);
+    debug_assert!(b < NP,"add(a,b) arg b = {} >= NP = {}",b,NP);
+    (a + b) % NP
+}
+
+#[inline]
+fn sub(a: usize, b: usize) -> usize {
+    debug_assert!(a < NP,"sub(a,b) arg a = {} >= NP = {}",a,NP);
+    debug_assert!(b < NP,"sub(a,b) arg b = {} >= NP = {}",b,NP);
+    if a >= b {
+        a - b
+    } else {
+        NP + a - b
+    }
+}
+
+impl Mancala {
+    pub fn new() -> Self {
+        let mut pit = [0;NP];
+        for p in 0..NP {
+            if (p == LB) || (p == RB) {
+                pit[p] = 0;
+            } else {
+                pit[p] = 4;
+            }
+        }
+        Mancala {pit, side: Player::R}
+    }
+    
+
+    #[allow(dead_code)]
+    fn heuristic(&self) -> f32 {
+        let (fb,eb) = match self.side {
+            Player::L => (LB,RB),
+            Player::R => (RB,LB),
+        };
+        let fs = self.pit[fb];
+        let es = self.pit[eb];
+
+        if self.terminal() {
+            if fs > es {
+                1.0
+            } else if fs == es {
+                0.5
+            } else {
+                0.0
+            }
+        } else {
+            let d = (fs - es) as f32;
+            let n = NS as f32;
+            0.5*(1.0 + d/n)
+        }
+    }
+    
+    fn winner(&self) -> Option<Player> {
+        let l = self.pit[LB];
+        let r = self.pit[RB];
+        if l == r {
+            None
+        } else if l > r {
+            Some(Player::L)
+        } else {
+            Some(Player::R)
+        }
+    }
+    
+
+    #[allow(dead_code)]
+    pub fn load(moves: &[Pit]) -> Mancala {
+        let mut g = Self::new();
+        for m in moves {
+            println!("{}",g);
+            g = g.make(*m);
+        }
+        println!("{}",g);
+        g
+    }
+
+    fn terminal(&self) -> bool {
+        (self.pit[LB] + self.pit[RB]) == NS as u8
+    }
+    
+    #[allow(dead_code)]
+    fn hash(&self) -> u64 {
+        let mut s = 0;
+        for p in 0..NP {
+            let n = self.pit[p] as usize;
+            let z = p*NS + n;
+            debug_assert!(
+                if z < (NS*NP) {
+                    true
+                } else {
+                    false
+                }
+            );
+            s ^= ZTABLE[z];
+        }
+        
+        let t = match self.side {
+            Player::L => 0,
+            Player::R => ZTURN,
+        };
+
+        t ^ s
+    }
+}
+
+impl Action for Pit {}
+impl arbor::Player for Player {}
+
+impl GameState<Player,Pit> for Mancala {
+    
+
+    fn make(&self, pit: Pit) -> Self {
+        debug_assert!(pit != RBank, "cannot choose right player bank");
+        debug_assert!(pit != LBank, "cannot choose left player bank");
+
+        let mut p = pit as usize;
+        
+        let mut next = *self;
+        let fbank = match self.side {
+            Player::L => LB,
+            Player::R => RB,
+        };
+        debug_assert!(sub(fbank,p) < NP/2, "cannot choose opposite side pit");
+        
+        debug_assert!(NP % 2 == 0, "cannot have an odd number of pits");
+        let ebank = add(fbank,NP/2);
+        
+        let mut n = self.pit[p];
+        
+        
+        debug_assert!({
+            if n == 0 {
+                println!("{}",self);
+                false
+            } else {
+                true
+            }
+        },"cannot choose pit without stones");
+        
+        
+        next.pit[p] = 0;
+        
+        loop {
+            p = add(p,1);
+            if p == ebank {
+                continue;
+            }
+            
+            next.pit[p] += 1;
+            
+            n -= 1;
+            if n == 0 {
+                break;
+            }
+        }
+        
+        let free_move = p == fbank;
+        let df = sub(fbank,p);
+        let capture = (df < NP/2) && (next.pit[p] == 1);
+        
+        if free_move {
+            next.side = self.side;
+        } else {
+            next.side = self.side.other();
+        }
+        
+        if capture && !free_move {
+            let o = add(fbank,df);
+            if next.pit[o] > 0 {
+                next.pit[fbank] += next.pit[o] + 1;
+                next.pit[o] = 0;
+                next.pit[p] = 0;
+            }
+        }
+        let f1 = add(ebank,1);
+        let e1 = add(fbank,1);
+        let fsum = next.pit[f1..fbank].iter().fold(0,|sum,x| sum + x);
+        let esum = next.pit[e1..ebank].iter().fold(0,|sum,x| sum + x);
+        
+        if fsum == 0 {
+            next.pit[ebank] += esum;
+            next.pit[e1..ebank].iter_mut().for_each(|p| *p = 0);
+        }
+        
+        if esum == 0 {
+            next.pit[fbank] += fsum;
+            next.pit[f1..fbank].iter_mut().for_each(|p| *p = 0);
+        }
+        
+        next
+    }
+
+    
+    fn actions<F>(&self,f: &mut F) where F: FnMut(Pit) {
+        let pits = match self.side {
+            Player::L => (L1 as usize)..(LBank as usize),
+            Player::R => (R1 as usize)..(RBank as usize),
+        };
+
+        for p in pits {
+            if self.pit[p] > 0 {
+                f(PIT[p]);
+            }
+        }
+    }
+    
+    
+
+    fn gameover(&self) -> Option<GameResult> {
+        if self.terminal() {
+            if let Some(winner) = self.winner() {
+                Some(if self.side == winner {GameResult::Win} else {GameResult::Lose})
+            } else {
+                Some(GameResult::Draw)
+            }
+        } else {
+            None
+        }
+    }
+
+
+    fn player(&self) -> Player {
+        self.side
+    }
+}
+
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use std::time::Duration;
+
+    fn best(moves: &[Pit]) -> Pit {
+        let game = Mancala::load(&moves);
+        let t = Duration::new(1,0);
+        let mut mcts = MCTS::new(&game);
+        let (action,_value,_error) = *mcts
+            .search(t)
+            .iter()
+            .max_by(|(_,w1,_),(_,w2,_)| {
+                if w1 > w2 {
+                    std::cmp::Ordering::Greater
+                } else {
+                    std::cmp::Ordering::Less
+                }
+            })
+            .expect("should have found a best move");
+        action
+    }
+
+    #[test]
+    fn mancala_free_move_1() {
+        let mut game = Mancala::new();
+        println!("{}",game);
+
+        game = game.make(Pit::R3);
+        println!("{}",game);
+
+        game = game.make(Pit::R6);
+        println!("{}",game);
+
+        assert!(game.side == super::Player::L);
+        assert!(game.pit[RB] == 2);
+        assert!(game.pit[LB] == 0);
+    }
+
+    #[test]
+    fn mancala_free_move_2() {
+        let mut game = Mancala::new();
+        println!("{}",game);
+        
+        game = game.make(Pit::R3);
+        println!("{}",game);
+
+        game = game.make(Pit::R6);
+        println!("{}",game);
+
+        game = game.make(Pit::L2);
+        println!("{}",game);
+
+        game = game.make(Pit::L6);
+        println!("{}",game);
+
+
+        assert!(game.side == super::Player::R);
+        assert!(game.pit[RB] == 2);
+        assert!(game.pit[LB] == 2);
+    }
+
+    #[test]
+    fn mancala_right_capture() {
+        let mut game = Mancala::new();
+        println!("{}",game);
+
+        game = game.make(Pit::R6);
+        println!("{}",game);
+        
+        game = game.make(Pit::L6);
+        println!("{}",game);
+        
+        game = game.make(Pit::R1);
+        println!("{}",game);
+        
+        assert!(game.side == super::Player::L);
+        assert!(game.pit[RB] == 1 + 1 + 5);
+        assert!(game.pit[LB] == 1);
+        assert!(game.pit[Pit::R6 as usize] == 0);
+        assert!(game.pit[Pit::L1 as usize] == 0);
+    }
+
+    #[test]
+    fn mancala_left_capture() {
+        let mut game = Mancala::new();
+        println!("{}",game);
+
+        game = game.make(Pit::R6);
+        println!("{}",game);
+        
+        game = game.make(Pit::L2);
+        println!("{}",game);
+        
+        game = game.make(Pit::L6);
+        println!("{}",game);
+        
+        game = game.make(Pit::R5);
+        println!("{}",game);
+        
+        game = game.make(Pit::L5);
+        println!("{}",game);
+        
+        game = game.make(Pit::R2);
+        println!("{}",game);
+        
+        game = game.make(Pit::L3);
+        println!("{}",game);
+        
+        game = game.make(Pit::R1);
+        println!("{}",game);
+        
+        game = game.make(Pit::L2);
+        println!("{}",game);
+        
+        assert!(game.side == super::Player::R);
+        assert!(game.pit[RB] == 4);
+        assert!(game.pit[LB] == 12);
+        assert!(game.pit[Pit::R4 as usize] == 0);
+        assert!(game.pit[Pit::L3 as usize] == 0);
+    }
+
+    #[test]
+    fn mancala_best_move_split() {
+        let m = best(&[R6,L6]);
+        assert!((m == R1) || (m == R2));
+    }
+
+    #[test]
+    fn mancala_best_move_free_turn() {
+        let m = best(&[R6,L6,R2]);
+        assert!(m == R6);
+    }
+
+}
