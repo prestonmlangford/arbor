10cc876cc6ee06788fb567283049f531a4a9e99a
Preston Langford <prestonmlangford@gmail.com>
2024-02-03 13:40:48 -0600

    Fix typo

--- c/arbor.c
+++ c/arbor.c
@@ -3,13 +3,13 @@
  *                                                                          *
  *   This file is part of Arbor.                                            *
  *                                                                          *
- *   Arbor is free software: you can redistribute it and/or modify it         *
+ *   Arbor is free software: you can redistribute it and/or modify it       *
  *   under the terms of the GNU Lesser General Public License as published  *
  *   by the Free Software Foundation, either version 3 of the License, or   *
  *   (at your option) any later version.                                    *
  *                                                                          *
  *   Arbor is distributed in the hope that it will be useful,               *
- *   but WITHOUT ANY WARRANTY; without even the impied warranty of         *
+ *   but WITHOUT ANY WARRANTY; without even the impied warranty of          *
  *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
  *   GNU Lesser General Public License for more details.                    *
  *                                                                          *
