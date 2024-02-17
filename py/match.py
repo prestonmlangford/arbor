5bb320e256053280b20fcb3ed42ffd5d9a356836
Preston Langford <prestonmlangford@gmail.com>
2024-02-17 12:52:02 -0600

    add tqdm to match

--- py/match.py
+++ py/match.py
@@ -1,5 +1,6 @@
 import multiprocessing
 from game import Game
+from tqdm import tqdm
 
 def pair_match(info):
     p1,p2,depth = info
@@ -19,9 +20,14 @@ def tournament(p1,p2,start,rounds):
         yield pair_match((p1,p2,start))
 
 def mp_tournament(p1,p2,start,rounds):
-    match_info = [(p1,p2,start)]*rounds
-    pool = multiprocessing.Pool()
-    return pool.map(pair_match,match_info)
+    with multiprocessing.Pool() as pool:
+        match_info = [(p1,p2,start)]*rounds
+        it = pool.imap_unordered(pair_match, match_info)
+
+        # tqdm will display a progress bar while this work is being done
+        results = list(tqdm(it, total=rounds))
+
+        return results
 
 # Reversi Perft
 # [DEPTH] [NUM LEAF NODES]
@@ -47,13 +53,13 @@ if __name__ == '__main__':
     p2 = {
         "name" : "andy",
         "path" : "target/release/reversi",
-        "time" : 1000
+        "time" : 100
     }
 
     p1 = {
         "name" : "fred",
-        "path" : "c/build/release/bin/reversi",
-        "time" : 1000
+        "path" : "c/build/osx/bin/reversi",
+        "time" : 100
     }
 
     # results: with fred expansion 10, andy expansion 0
@@ -66,7 +72,7 @@ if __name__ == '__main__':
 
     andy = 0
     fred = 0
-    for result in mp_tournament(p1,p2,5,2):
+    for result in mp_tournament(p1,p2,5,16):
         p1, p2 = result
         if p1 == "andy":
             andy += 1
