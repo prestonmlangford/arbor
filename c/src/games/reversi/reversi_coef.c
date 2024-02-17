7a87760841252be912c6c4cba02fe4baa572b4ce
Preston Langford <prestonmlangford@gmail.com>
2024-02-17 09:14:34 -0600

    add coeff file

---
+++ c/src/games/reversi/reversi_coef.c
@@ -0,0 +1,3 @@
+#include "reversi_coef.h"
+
+float reversi_heuristic_coef[NUM_SETS][NUM_FEAT] = {};
---
+++ c/src/games/reversi/reversi_coef.h
@@ -0,0 +1,8 @@
+#ifndef REVERSI_COEF_H
+#define REVERSI_COEF_H
+
+#define NUM_SETS 60
+#define NUM_FEAT 3
+extern float reversi_heuristic_coef[NUM_SETS][NUM_FEAT];
+
+#endif // REVERSI_COEF_H
